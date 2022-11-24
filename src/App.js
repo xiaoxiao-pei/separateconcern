6a6ceba84e1dc5146770a4ff981da3edb1d32293
xiaoxiao-pei <pxtoday@hotmail.com>
2022-11-24 10:41:15 -0500

    seperate clock

--- src/App.js
+++ src/App.js
@@ -8,15 +8,9 @@ function DisplayMessage(props) {
   return <h1 style={{ color: props.color }}>Hello React World</h1>;
 }
 
-function Clock(props) {
-  return <p> React Clock: {props.time} </p>;
-}
-
 function toggle(color) {
   if (color === "blue") {
     return "red";
-  } else if (color === "red") {
-    return "green";
   } else {
     return "blue";
   }
@@ -25,23 +19,16 @@ function toggle(color) {
 function App() {
   const [color, setColor] = React.useState(JSON.parse(sessionStorage.getItem("color")) || "blue");
   const [count, setCount] = React.useState(JSON.parse(localStorage.getItem("count")) || 0);
-  const [time, setTime] = React.useState(new Date().toLocaleTimeString());
-
-  console.log(color);
-  console.log(count);
 
   React.useEffect(() => {
     sessionStorage.setItem('color', JSON.stringify(color));
     localStorage.setItem('count', JSON.stringify(count));
-    const interval = setInterval(() => setTime(new Date().toLocaleTimeString()), 3000);
-    return () => { clearInterval(interval); }
-
   }, [color, count]);
 
 
   return (<div>
     <DisplayMessage color={color} />
-    <Clock time={time} />
+    <Clock />
     <p>{count}</p>
     <button onClick={() => {
       setColor(toggle(color));
@@ -51,11 +38,17 @@ function App() {
     </button>
   </div>
   );
+
+  function Clock() {
+    const [time, setTime] = React.useState(new Date().toLocaleTimeString());
+    //setInterval(() => setTime(new Date().toLocaleTimeString()), 3000);
+    React.useEffect(() => {
+      const interval = setInterval(() =>
+        setTime(new Date().toLocaleTimeString()), 3000);
+      return () => { clearInterval(interval); }
+    }, [time]);
+    return <p>React Clock: {time} </p>
+  }
 }
-
-//const root = ReactDOM.createRoot(document.getElementById("root"));
-//root.render(<App />);
-//setInterval(() => root.render(<App />), 1000);
-
 
 export default App;
