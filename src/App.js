a3f195f44d0f05de01bf08fc320fafc37b461c6b
xiaoxiao-pei <pxtoday@hotmail.com>
2022-11-24 09:46:07 -0500

    first react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React from 'react';
+//import ReactDOM from 'react-dom';
+
+
+function DisplayMessage(props) {
+  return <h1 style={{ color: props.color }}>Hello React World</h1>;
+}
+
+function Clock(props) {
+  return <p> React Clock: {props.time} </p>;
+}
+
+function toggle(color) {
+  if (color === "blue") {
+    return "red";
+  } else if (color === "red") {
+    return "green";
+  } else {
+    return "blue";
+  }
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const [color, setColor] = React.useState(JSON.parse(sessionStorage.getItem("color")) || "blue");
+  const [count, setCount] = React.useState(JSON.parse(localStorage.getItem("count")) || 0);
+
+  console.log(color);
+  console.log(count);
+
+  React.useEffect(() => {
+    sessionStorage.setItem('color', JSON.stringify(color));
+    localStorage.setItem('count', JSON.stringify(count));
+  }, [color, count]);
+
+
+  return (<div>
+    <DisplayMessage color={color} />
+    <Clock time={new Date().toLocaleTimeString()} />
+    <p>{count}</p>
+    <button onClick={() => {
+      setColor(toggle(color));
+      setCount(count + 1);
+    }}>
+      Click me React
+    </button>
+  </div>
   );
 }
+
+//const root = ReactDOM.createRoot(document.getElementById("root"));
+//root.render(<App />);
+//setInterval(() => root.render(<App />), 1000);
+
 
 export default App;
