bce9a95e186db352e388019b6f1908017261931e
xiaoxiao-pei <pxtoday@hotmail.com>
2022-11-25 17:10:54 -0500

    bootstrap dropdown navs

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,30 @@
 import '../css/header.css'
 import React from "react";
-//import { NavLink } from "react-router-dom";
-import HomeButton from "./HomeButton";
-//import NavButton from './NavButton';
+import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
 
 function Header() {
     return (
-        <div class= "App-header">
-             <div id = "header-menu">
-                <HomeButton />
-            </div> 
-            {/* <div >
-                <NavButton to="/" label="Home" />
-                <NavButton to="/about" label="About" />
-                <NavButton to="/contact" label="Contact" />
-            </div> */}
+        <div >
+            <Nav variant="tabs" defaultActiveKey="/home" as="ul">
+                <Nav.Item as="li">
+                    <Nav.Link href="/">Home</Nav.Link>
+                </Nav.Item>
+                <Nav.Item as="li">
+                    <Nav.Link eventKey="link-1" href="/about">About</Nav.Link>
+                </Nav.Item>
+                <Nav.Item as="li">
+                    <Nav.Link eventKey="link-2" href="/contact">Contact</Nav.Link>
+                </Nav.Item>
+                <NavDropdown title="Account" id="nav-dropdown">
+                    <NavDropdown.Item eventKey="4.1">Your Profile</NavDropdown.Item>
+                    <NavDropdown.Item eventKey="4.2">Your Credits</NavDropdown.Item>
+                    <NavDropdown.Item eventKey="4.3">Your projects</NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item eventKey="4.4">Sign out</NavDropdown.Item>
+                </NavDropdown>
+            </Nav>
         </div>
     );
 }
