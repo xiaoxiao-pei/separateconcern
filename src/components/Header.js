1ef0fdd88b1047d80ac14d3ba27186d9a85d8f76
xiaoxiao-pei <pxtoday@hotmail.com>
2022-11-25 20:05:17 -0500

    bootstrap dropdown navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,29 +2,37 @@ import '../css/header.css'
 import React from "react";
 import Nav from 'react-bootstrap/Nav';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
 
 
 function Header() {
+    
     return (
         <div >
-            <Nav variant="tabs" defaultActiveKey="/home" as="ul">
-                <Nav.Item as="li">
-                    <Nav.Link href="/">Home</Nav.Link>
-                </Nav.Item>
-                <Nav.Item as="li">
-                    <Nav.Link eventKey="link-1" href="/about">About</Nav.Link>
-                </Nav.Item>
-                <Nav.Item as="li">
-                    <Nav.Link eventKey="link-2" href="/contact">Contact</Nav.Link>
-                </Nav.Item>
-                <NavDropdown title="Account" id="nav-dropdown">
-                    <NavDropdown.Item eventKey="4.1">Your Profile</NavDropdown.Item>
-                    <NavDropdown.Item eventKey="4.2">Your Credits</NavDropdown.Item>
-                    <NavDropdown.Item eventKey="4.3">Your projects</NavDropdown.Item>
-                    <NavDropdown.Divider />
-                    <NavDropdown.Item eventKey="4.4">Sign out</NavDropdown.Item>
-                </NavDropdown>
-            </Nav>
+            <Navbar collapseOnSelect expand="md" bg="dark" variant="dark">
+                <Container>
+                    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link href="/"> Home</Nav.Link>
+                            <Nav.Link href="/about">About</Nav.Link>
+                            <Nav.Link href="/contact">Contact</Nav.Link>
+                            <NavDropdown title="Account" id="collasible-nav-dropdown">
+                                <NavDropdown.Item href="#">Your profile</NavDropdown.Item>
+                                <NavDropdown.Item href="#">Your projects</NavDropdown.Item>
+                                <NavDropdown.Item href="#">Your credits</NavDropdown.Item>
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item href="#">Log out </NavDropdown.Item>
+                            </NavDropdown>
+                        </Nav>
+                        <Nav>
+                            <Nav.Link href="#deets">Log out</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
         </div>
     );
 }
